afb8eb94cfc7fbb2b620e39d4d4a405bcbbf734a
Dila <83170271+tatertotsey@users.noreply.github.com>
2022-05-14 14:35:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    let random = Math.floor(Math.random() * 3);
+
+    switch (random) {
+        case 0: return myArray[0];
+
+        case 1: return myArray[1];
+
+        case 2: return myArray[2];
+    }
+}
+//console.log(computerPlay());
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+
+    if (playerSelection != computerSelection) {
+        if (playerSelection == "rock" & computerSelection == "scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+
+        if (playerSelection == "scissors" & computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+
+        if (playerSelection == "paper" & computerSelection == "rock") {
+            return "You Win! Paper beats Rock";
+        }
+
+        if (playerSelection == "rock" & computerSelection == "paper") {
+            return "You Lose! Paper beats Rock";
+        }
+
+        if (playerSelection == "paper" & computerSelection == "scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
+
+        if (playerSelection == "scissors" & computerSelection == "paper") {
+            return "You Win! Scissors beats Paper";
+        }
+
+    }
+} 
+//console.log("PlayerSelection> " + playerSelection);
+//console.log("Computerselection> " + computerSelection);
+//console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let player = 0;
+    let computer = 0;
+    let listresult = [];
+
+    for (let round = 0; round < 5; round++) {
+       var playerSelection = "paper";
+       //var playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+        var computerSelection = computerPlay().toLowerCase();
+        listresult = playRound(playerSelection, computerSelection).split(" ");
+
+        console.log(listresult);
+
+        if (listresult[1] == "Win!") {
+            player += 1;
+        }
+
+        if (listresult[1] == "Lose!") {
+            computer += 1;
+        }
+
+        listresult = [];
+    }
+    let result = "Player: " + player.toString() + " Computer: " + computer.toString();
+    return result
+}
+console.log(game());
